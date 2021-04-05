7c3751aea66704a35b64cc2faaa4cbe1089a222b
zakgrin <zakariya.abugrin@gmail.com>
2021-04-05 14:28:36 +0200

    Unit testing

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import requests
 import unittest
 import ast
+import argparse
 
 
 class TestApp(unittest.TestCase):
@@ -30,7 +31,41 @@ class TestApp(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    url_endpoint = 'predict'
-    url = 'http://127.0.0.1:8080/' + url_endpoint
-    print('URL:', url, '(make sure that app.py is running in this host)')
+    url_endpoint = '/predict'
+    host_option = 'cloud'
+    if host_option == 'local':
+        url = 'http://127.0.0.1:8080' + url_endpoint
+    if host_option == 'cloud':
+        url = 'https://auto-mpg-predict-jdywf5v6pa-uc.a.run.app' + url_endpoint
     unittest.main()
+
+    # Todo:
+    # parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    # parser.add_argument('-ep', '--endpoint', default='/predict', metavar='',
+    #                     help="API endpoint (default: /predict)")
+    # subparser = parser.add_subparsers(help='selection', dest='host_option')
+    # # local:
+    # local_parser = subparser.add_parser('local', aliases=['l'],
+    #                                     help='local api option (machine or docker)',
+    #                                     formatter_class=argparse.RawTextHelpFormatter)
+    # local_parser.add_argument('-p', '--port', default='8080', type=str,
+    #                           help="Local API port (default: 8080)")
+    # local_parser.add_argument('-host', '--host', default='http://127.0.0.1', type=str, metavar='',
+    #                           help="Local API host (default: http://127.0.0.1)")
+    # # cloud:
+    # cloud_parser = subparser.add_parser('cloud', aliases=['c'],
+    #                                     help='cloud api option (GCP)',
+    #                                     formatter_class=argparse.RawTextHelpFormatter)
+    # cloud_parser.add_argument('-host', '--host',
+    #                           default='https://auto-mpg-predict-jdywf5v6pa-uc.a.run.app',
+    #                           type=str, metavar='',
+    #                           help="Cloud API host (default: https://auto-mpg-predict-jdywf5v6pa-uc.a.run.app)")
+    # args = parser.parse_args()
+    # print(args)
+    # url_endpoint = '/predict'
+    # if args.host_option == 'local':
+    #     url = 'http://127.0.0.1:8080' + url_endpoint
+    # if args.host_option == 'cloud':
+    #     url = 'https://auto-mpg-predict-jdywf5v6pa-uc.a.run.app' + url_endpoint
+    #
+    # print('URL:', url, '(make sure that app.py is running in this host)')
